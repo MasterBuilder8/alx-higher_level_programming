626a818355c16cd2eaf782d366a7db7202a42bb5
MasterBuilder8 <collins.snilloc9@gmail.com>
2023-03-13 08:26:02 -0700

    remove an element

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for c and C in my_string:
-        my_string.remove(c)
-        my_string.remove(C)
+    new_str = ''
+
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_str += char
+
+            return new_str
