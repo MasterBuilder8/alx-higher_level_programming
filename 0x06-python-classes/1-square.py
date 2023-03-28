f89c5f09dd4474f88082cebd448619e9a732eeb6
MasterBuilder8 <collins.snilloc9@gmail.com>
2023-03-28 11:16:04 -0700

    class and objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Square class
+
+square with size
+
+"""
+
+class Square:
+
+    def __init__(self, size):
+        """init
+        the __init__ method creates the size value of the square
+
+        attributes:
+        size (int): size of the square
+        """
+
+        self.__size = size
