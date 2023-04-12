a7f8621d00fc2e26058e6c1dfc01c7e55de4d55d
MasterBuilder8 <collins.snilloc9@gmail.com>
2023-04-12 04:04:39 -0700

    inheritance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""a function that checks if an object is an instance of, or if the object is an instance of a class"""
+"""defining a funnction"""
 
 
 def is_kind_of_class(obj, a_class):
-    """function that returns True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class ; otherwise False
-    """
+    """function to check if an object is an instance of  a class"""
 
     if type(obj) == a_class:
         return True
