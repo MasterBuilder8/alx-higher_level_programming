0bcb981ec5ac0577fada1acd118e8dc19b3aadd4
MasterBuilder8 <collins.snilloc9@gmail.com>
2023-04-13 11:42:04 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,7 +4,7 @@ defining a function
 """
 
 def read_file(filename=""):
-    """function that reads a text file"""
+    """function that reads a text file and prints to stdout"""
     with open(filename, 'r', encoding='utf-8') as f:
         for line in f:
             print(line, end='')
