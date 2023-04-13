5ba975f83c6e33af64e0f0439e5cbb3cbc982b53
MasterBuilder8 <collins.snilloc9@gmail.com>
2023-04-13 06:25:38 -0700

    inheritance

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module to define a subclass"""
+
+BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class that inherites from BaseGeometry class
+    
+    Attributes:
+    width and height
+    """
+def __init__(self, width, height):
+    """instantiation of width and height"""
+    self.__width = width
+    self.__height = height
+    self.integer_validator("width", self.__width)
+    self.integer_validator("height", self.__height)
---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module to define subclass"""
+BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class which inherits from BaseGeometry class"""
+
+    def __init__(self, width, height):
+        """instantiation of width and height"""
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
+
+        def area(self):
+            """computes the area of the Rectangle instance"""
+            return self.__width * self.__height
+
+        def __str__(self):
+            """returns the string to print"""
+            return "[Rectangle] {}/{}".format(self.__width, self.__height)
