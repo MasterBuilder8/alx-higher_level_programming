9ace53e2767b8afe272e972580374b1cb0a4d87c
MasterBuilder8 <collins.snilloc9@gmail.com>
2023-06-12 14:17:52 -0700

    second biggest

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-const argument = process.argv[2];
-const numOccurrences = parseInt(argument);
-
-if (!isNaN(numOccurrences)) {
-  for (let i = 0; i < numOccurrences; i++) {
-    console.log("C is fun");
+function factorial (a) {
+  if (isNaN(a) || a <= 1) {
+    return 1;
+  } else {
+    return a * factorial(a - 1);
   }
-} else {
-  console.log("Missing number of occurrences");
 }
 
+const num = Number.parseInt(process.argv[2]);
+const fin = factorial(num);
+console.log(fin);
